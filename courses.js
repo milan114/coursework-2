0b168631dc9c25df888b0e3d7acbe7a3e71dea81
milan114 <milanhirani@gmail.com>
2020-02-08 13:53:20 +0000

    Add files via upload

---
+++ courses.js
@@ -0,0 +1,12 @@
+var courses = [
+    { 'slug': 'maths', 'topic': 'math', 'location': 'hendon', 'price': 100 },
+    { 'slug': 'maths', 'topic': 'math', 'location': 'colindale', 'price': 80 },
+    { 'slug': 'maths', 'topic': 'math', 'location': 'brent cross', 'price': 90 },
+    { 'slug': 'maths', 'topic': 'math', 'location': 'golders green', 'price': 120 },
+    { 'slug': 'english', 'topic': 'english', 'location': 'hendon', 'price': 110 },
+    { 'slug': 'english', 'topic': 'english', 'location': 'colindale', 'price': 90 },
+    { 'slug': 'english', 'topic': 'english', 'location': 'brent cross', 'price': 90 },
+    { 'slug': 'english', 'topic': 'english', 'location': 'golders green', 'price': 130 },
+    { 'slug': 'piano', 'topic': 'piano', 'location': 'hendon', 'price': 120 },
+    { 'slug': 'piano', 'topic': 'piano', 'location': 'golders green', 'price': 140 }
+]
